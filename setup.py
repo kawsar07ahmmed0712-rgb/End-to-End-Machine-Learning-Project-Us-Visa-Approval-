eb9126746211ed1dd7f9d1b97d9a005645ae8cbd
Kawsar Ahmmed <kawsar07ahmmed0712@gmail.com>
2025-10-20 16:45:51 +0600

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "us_visa",
+    version = "0.0.0",
+    author="Kawsar",
+    author_email="kawsar07ahmmed0712@gmail.com",
+    packages= find_packages()
+)
